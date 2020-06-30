0312dfe1cf2bf266eef9c75a429661daff2d16d9
tommylay1903@gmail.com <tommylay1903@gmail.com>
2020-06-30 10:57:50 -0700

    Add project files.

---
+++ SimpleAsyncEx/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimpleAsyncExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var calculate = CalculationAsync();
+            Console.WriteLine("While CalculationsAsync is running, I can do other stuff here");
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine("doing other stuff");
+            }
+            int result = await calculate;
+            Console.WriteLine(result);
+
+        }
+
+        static async Task<int> CalculationAsync()
+        {
+            await Task.Delay(3000);
+            Console.WriteLine("Completed Calculation");
+            return 1;
+        }
+    }
+}
---
+++ SyncEx/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SyncEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Step 1: You are in main thread");
+            CallTimeConsumeTask();
+            MyMethod();
+            Console.WriteLine("Step 8: Main thread ended");
+            Console.ReadLine();
+        }
+
+        static void CallTimeConsumeTask()
+        {
+            Console.WriteLine("Step 2: CallTimeConsumeTask() started");
+            TimeConsumingTask();
+            Console.WriteLine("Step 5: CallTimeConsumeTask() ended");
+        }
+        static void TimeConsumingTask()
+        {
+            Console.WriteLine("Step 3: TimeConsumingTask() started,waiting 5 seconds..");
+            Thread.Sleep(5000);
+            Console.WriteLine("Step 4: TimeConsumingTask() ended");
+        }
+        static void MyMethod()
+        {
+            Console.WriteLine("Step 6: Doing some work in MyMethod()");
+            for (var i = 0; i <= 5; i++)
+            {
+                for (var j = i; j <= 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Step 7: MyMethod() ended");
+        }
+    }
+}
